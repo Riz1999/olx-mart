0f13bf08d578f957995628538c46b3c80f537f37
rizwan zhad <92221232+Riz1999@users.noreply.github.com>
2023-06-15 14:11:58 +0530

    Update settings.py

--- BS/settings.py
+++ BS/settings.py
@@ -24,7 +24,7 @@ DATABASE_URL ="postgresql://postgres:Tb5aHcwv6NQ0IOLzIIT9@containers-us-west-190
 SECRET_KEY = '_!3=&0ga*0isp#@5f)qj5nd#8xg74of5h&!$6pm+z55!0n)ox$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
